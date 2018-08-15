3a1730ef5e58a2a5c869902e9e4b360735f85035
Michal Bandyszak <michalbandyszak@gmail.com>
2018-08-15 19:54:33 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+
+app.get('/', function(req, res) {	
+	res.render('login');
+
+});
+
+app.get('/auth/google', function(req, res){
+	res.render('signed');
+});
+
+
+app.listen(3000);
+app.use(function(req, res) {
+	res.status(404).send("Wybacz, nie mogliśmy znaleźć tego, czego żądasz! :( ");
+});
